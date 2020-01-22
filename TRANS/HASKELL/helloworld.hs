852dea44fb789a2c666002fa42c2faeca842e912
ac2epsilon <ac2epsilon@gmail.com>
2020-01-22 02:46:03 +0200

    Akka Example app

--- TRANS/HASKELL/helloworld.hs
+++ TRANS/HASKELL/helloworld.hs
@@ -4,3 +4,23 @@ import Data.Char
 main = forever $ do  
     l <- getLine  
     putStrLn $ map toUpper l
+
+data BookInfo = Book Int String [String]
+                deriving (Show)
+
+data MagazineInfo = Magazine Int String [String]
+                    deriving (Show)
+
+myInfo = Book 9780135072455 "Algebra of Programming"
+         ["Richard Bird", "Oege de Moor"]
+
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+type CustomerID = Int
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                 | CashOnDelivery
+                 | Invoice CustomerID
+                   deriving (Show)
+        
